1e84dd47d6a68bb90ce6be80cf38151d94c2fce4
Ira Miller <public@iramiller.com>
2018-07-01 13:38:11 -0500

    v1.1.0

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
+const foreach = require('guld-fs-foreach')
 const spawn = require('guld-spawn')
-const { getJS } = require('guld-env')
 
 async function strReplace (oldstr, newstr, args) {
-  if (getJS().startsWith('node')) {
-    var found = await spawn('find', '', [...args, '-type', 'f'])
-    await Promise.all(found.split('\n').map(async f => {
-      if (f === '') return
-      return spawn('sed', '', ['-i', `s/${oldstr}/${newstr}/g`, f])
-    }))
-  } else throw new Error('This function only available in node for now.')
+  return foreach(args[0], async f => {
+    if (f === '') return
+    return spawn('sed', '', ['-i', `s/${oldstr}/${newstr}/g`, f])
+  }, args.slice(1))
 }
 
 module.exports = strReplace

1e8a0f2e1d9471caa3abbf6c42c2b8e5891a3d47
Ira Miller <public@iramiller.com>
2018-09-03 12:03:23 -0500

    deps

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const foreach = require('guld-fs-foreach')
-const spawn = require('guld-spawn')
+const spawn = require('guld-spawn').getSpawn()
 
 async function strReplace (oldstr, newstr, args) {
   return foreach(args[0], async f => {
     if (f === '') return
-    return spawn('sed', '', ['-i', `s/${oldstr}/${newstr}/g`, f])
-  }, args.slice(3))
+    var resp = await spawn('sed', '', ['-i', `s/${oldstr}/${newstr}/g`, f], true)
+    return resp
+  }, args.slice(1))
 }
 
 module.exports = strReplace
